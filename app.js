957ed44f4769ea693e6bfb38553eb935f7c44621
Ishan Udgirkar <udgirkarishan@gmail.com>
2022-09-02 11:09:53 +0530

    Near to success

--- app.js
+++ app.js
@@ -20,17 +20,28 @@ function calculateProfitAndLoss(initial , quantity , current) {
     if(initial > current) {
     var loss = (initial - current) * quantity ; 
     var lossPercentage = (loss / initial) * 100 ;
-    console.log(`Hey the loss is ${loss} and the percent is ${lossPercentage}% ` );
+
+    showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}% `);
+     
 }else if(current > initial){
     //Profit logic here
     var profit = (current - initial) * quantity;
     var profitPercentage = (profit / initial) * 100 ;
-    console.log(`Hey the profit is ${profit} and the percent is ${profitPercentage}% ` );
+   
 
+    showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}% `);
+    
 }else{
     //the rest logic here
-    console.log(`no pain no gain and no gain no pain`);
+    showOutput(`no pain no gain and no gain no pain`);
+ 
 }
+
+}
+
+function showOutput(message){
+   
+    outputBox .innerHTML = message;
 
 }
 
@@ -38,6 +49,3 @@ function calculateProfitAndLoss(initial , quantity , current) {
 submitBtn.addEventListener('click',submitHandler);
 
 
-calculateProfitAndLoss(100,10,10);
-calculateProfitAndLoss(10,10,100);
-calculateProfitAndLoss(10,10,10);
