293c4ba892fccb5efcae357165c23f1ff8f2fce8
Ishan Udgirkar <udgirkarishan@gmail.com>
2022-09-09 21:47:20 +0530

    optional 1 done

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ var submitBtn = document.querySelector('#submit-btn');
 var outputBox = document.querySelector('#output-box');
 
 function submitHandler(){
-    var ip = Number(initialPrice.value);
-    var qty = Number(stocksQuantity.value);
-    var curr = Number(currentPrice.value);
- 
-    calculateProfitAndLoss(ip,qty,curr);  
+    if (initialPrice.value && stocksQuantity.value && currentPrice.value) {
+        var ip = Number(initialPrice.value);
+        var qty = Number(stocksQuantity.value);
+        var curr = Number(currentPrice.value);
+    
+        calculateProfitAndLoss(ip, qty, curr);
+      }
+      else {
+        alert('Please fill out all Fields');
+      } 
 
 }
 
@@ -17,25 +22,31 @@ function submitHandler(){
 
 
 function calculateProfitAndLoss(initial , quantity , current) {
-    if(initial > current) {
-    var loss = (initial - current) * quantity ; 
-    var lossPercentage = (loss / initial) * 100 ;
-
-    showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}% `);
-     
-}else if(current > initial){
-    //Profit logic here
-    var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
-   
-
-    showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}% `);
+    if (initial > current) {
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+        var newLossPercentage = Number(lossPercentage.toFixed(2));
     
-}else{
-    //the rest logic here
-    showOutput(`no pain no gain and no gain no pain`);
- 
-}
+    
+        // showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}% `);
+        showOutput(`Hey the loss is ${loss} and the percent is ${newLossPercentage}% `);
+    
+      } else if (current > initial) {
+        //Profit logic here
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
+        var newProfitPercentage = Number(profitPercentage.toFixed(2));
+    
+    
+        // showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}% `);
+        showOutput(`Hey the profit is ${profit} and the percent is ${newProfitPercentage}% `);
+    
+      } else {
+        //the rest logic here
+        showOutput(`no pain no gain and no gain no pain`);
+    
+      }
+    
 
 }
 
