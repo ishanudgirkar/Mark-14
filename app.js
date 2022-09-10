5a1e3cdd84d09bad335f6819b4f0b2e75cb59158
Ishan Udgirkar <udgirkarishan@gmail.com>
2022-09-10 20:30:25 +0530

    Done with bonus !

--- app.js
+++ app.js
@@ -24,37 +24,43 @@ function submitHandler(){
 function calculateProfitAndLoss(initial , quantity , current) {
     if (initial > current) {
         var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / initial) * 100;
+        var lossPercentage = (loss / (initial * quantity)) * 100;
         var newLossPercentage = Number(lossPercentage.toFixed(2));
     
     
         // showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}% `);
-        showOutput(`Hey the loss is ${loss} and the percent is ${newLossPercentage}% `);
+        // showOutput(`Hey the loss is ${loss} and the percent is ${newLossPercentage}% `);
+        outputBox.innerText = `Hey the loss is ${loss} and the percent is ${newLossPercentage}% `;
+        outputBox.style.color = "red";
     
       } else if (current > initial) {
         //Profit logic here
         var profit = (current - initial) * quantity;
-        var profitPercentage = (profit / initial) * 100;
+        var profitPercentage = (profit / (initial * quantity)) * 100;
         var newProfitPercentage = Number(profitPercentage.toFixed(2));
     
     
         // showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}% `);
-        showOutput(`Hey the profit is ${profit} and the percent is ${newProfitPercentage}% `);
+        // showOutput(`Hey the profit is ${profit} and the percent is ${newProfitPercentage}% `);
+        outputBox.innerText = `Hey the profit is ${profit} and the percent is ${newProfitPercentage}% `;
+        outputBox.style.color = "green";
     
       } else {
         //the rest logic here
-        showOutput(`no pain no gain and no gain no pain`);
+        // showOutput(`no pain no gain and no gain no pain`);
+        outputBox.innerText = `no pain no gain and no gain no pain`;
+        outputBox.style.color = "black";
     
       }
     
 
 }
 
-function showOutput(message){
+// function showOutput(message){
    
-    outputBox .innerHTML = message;
+//     outputBox .innerHTML = message;
 
-}
+// }
 
 
 submitBtn.addEventListener('click',submitHandler);
